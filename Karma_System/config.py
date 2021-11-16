cd5154b911dba216ca0bde88e266ff9a4a122201
Kittuthemeow <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-11-16 15:20:56 +0530

    Add files via upload

---
+++ Karma_System/config.py
@@ -0,0 +1,14 @@
+from typing import Union, Optional
+
+API_ID: int = ""
+API_HASH: str = ""
+STRING_SESSION: str = ""
+Sibyl_logs: Union[str, int] = ""
+Sibyl_approved_logs: Union[str, int] = ""
+GBAN_MSG_LOGS: Optional[Union[str, int]] = ""
+BOT_TOKEN: str = ""
+ENFORCERS: int = ""
+INSPECTORS: int = ""
+
+MONGO_DB_URL: str = ""
+SIBYL: int = ""
