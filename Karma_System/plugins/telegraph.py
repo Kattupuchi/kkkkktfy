2f3e09614770e43cfa49e1df11189956949a048b
Kittuthemeow <89501967+KIRANRAJ6554@users.noreply.github.com>
2021-11-20 20:01:28 +0530

    Update telegraph.py

--- Karma_System/plugins/telegraph.py
+++ Karma_System/plugins/telegraph.py
@@ -8,7 +8,7 @@ from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 from telethon.utils import get_display_name
 
-from plugins import catub
+from karma_system import catub
 
 from ..Config import Config
 from ..core.logger import logging
