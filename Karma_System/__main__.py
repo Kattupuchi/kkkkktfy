f6b748c0a23c7ca0a9d1a76bc84d8a7c70edd63b
Kittu [ TEAM KARMA ] <89501967+kittuthemeow@users.noreply.github.com>
2022-04-01 17:53:51 +0530

    Update __main__.py

--- Karma_System/__main__.py
+++ Karma_System/__main__.py
@@ -111,7 +111,7 @@ async def main():
             msg += f"\n**{plugin}**\n\n`{FAILED_TO_LOAD[plugin]}`"
         await System.send_message(Karma_logs, msg)
     else:
-        await System.send_message("@karma_appeal", "[Karma Is Not Stopable👹](https://telegra.ph/file/99c4658f9fdbaa00d0aa3.mp4)", parse_mode=ParseMode.MARKDOWN)
+        await System.send_message("@karma_appeall", "[Karma Is Not Stopable👹](https://telegra.ph/file/99c4658f9fdbaa00d0aa3.mp4)", parse_mode=ParseMode.MARKDOWN)
     await System.run_until_disconnected()
 
 
